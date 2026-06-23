642bcdba5b757defa002285eb295c80cdee57327
svizcaino26 <svizcaino26@gmail.com>
2026-06-23 13:28:51 -0500

    freature: implement method to get all answers from a question on DB

--- src/persistance/answers_dao.rs
+++ src/persistance/answers_dao.rs
@@ -70,24 +70,23 @@ impl AnswersDao for AnswersDaoImpl {
     }
 
     async fn get_answers(&self, question_uuid: String) -> Result<Vec<AnswerDetail>, DBError> {
-        // Use the `sqlx::types::Uuid::parse_str` method to parse `question_uuid` into a `Uuid` type.
-        // parse_str docs: https://docs.rs/sqlx/latest/sqlx/types/struct.Uuid.html#method.parse_str
-        //
-        // If `parse_str` returns an error, map the error to a `DBError::InvalidUUID` error
-        // and early return from this function.
-        let uuid = todo!();
+        let uuid = sqlx::types::Uuid::try_parse(&question_uuid)
+            .map_err(|_| DBError::InvalidUUID(format!("Invalid UUID: {}", question_uuid)))?;
 
-        // Make a database query to get all answers associated with a question uuid.
-        // Here is the SQL query:
-        // ```
-        // SELECT * FROM answers WHERE question_uuid = $1
-        // ```
-        // If executing the query results in an error, map that error
-        // to a `DBError::Other` error and early return from this function.
-        let records = todo!();
+        let records = sqlx::query!(
+            r#"
+                SELECT * FROM answers WHERE question_uuid = $1
+            "#,
+            uuid
+        ).fetch_all(&self.db).await.map_err(|e| DBError::Other(Box::new(e)))?;
 
-        // Iterate over `records` and map each record to a `AnswerDetail` type
-        let answers = todo!();
+        let answers = records.into_iter()
+            .map(|record| AnswerDetail {
+                answer_uuid: record.answer_uuid.to_string(),
+                question_uuid: record.question_uuid.to_string(),
+                content: record.content,
+                created_at: record.created_at.to_string()})
+            .collect();
 
         Ok(answers)
     }
