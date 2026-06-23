47296c2c9db1893bdb8075b4af7b6f5d83c009ee
svizcaino26 <svizcaino26@gmail.com>
2026-06-22 22:29:58 -0500

    feature: implement method to delete answer from db

--- src/persistance/answers_dao.rs
+++ src/persistance/answers_dao.rs
@@ -25,27 +25,9 @@ impl AnswersDaoImpl {
 #[async_trait]
 impl AnswersDao for AnswersDaoImpl {
     async fn create_answer(&self, answer: Answer) -> Result<AnswerDetail, DBError> {
-        // Use the `sqlx::types::Uuid::parse_str` method to parse the `question_uuid` field
-        // in `Answer` into a `Uuid` type.
-        // parse_str docs: https://docs.rs/sqlx/latest/sqlx/types/struct.Uuid.html#method.parse_str
-        //
-        // If `parse_str` returns an error, map the error to a `DBError::InvalidUUID` error
-        // and early return from this function.
-        let uuid = sqlx::types::Uuid::parse_str(&answer.question_uuid)
+        let uuid = sqlx::types::Uuid::try_parse(&answer.question_uuid)
             .map_err(|_| DBError::InvalidUUID(format!("Invalid UUID: {}", answer.question_uuid)))?;
 
-        // Make a database query to insert a new answer.
-        // Here is the SQL query:
-        // ```
-        // INSERT INTO answers ( question_uuid, content )
-        // VALUES ( $1, $2 )
-        // RETURNING *
-        // ```
-        // If executing the query results in an error, check to see if
-        // the error code matches `postgres_error_codes::FOREIGN_KEY_VIOLATION`.
-        // If so early return the `DBError::InvalidUUID` error. Otherwise early return
-        // the `DBError::Other` error.
-        
         let record = sqlx::query!(
             r#"
                 INSERT INTO answers (question_uuid, content)
@@ -64,7 +46,6 @@ impl AnswersDao for AnswersDaoImpl {
                 }
                 e => DBError::Other(Box::new(e))
             })?;
-
             
         Ok(AnswerDetail {
             answer_uuid: record.answer_uuid.to_string(),
@@ -75,20 +56,15 @@ impl AnswersDao for AnswersDaoImpl {
     }
 
     async fn delete_answer(&self, answer_uuid: String) -> Result<(), DBError> {
-        // Use the `sqlx::types::Uuid::parse_str` method to parse `answer_uuid` into a `Uuid` type.
-        // parse_str docs: https://docs.rs/sqlx/latest/sqlx/types/struct.Uuid.html#method.parse_str
-        //
-        // If `parse_str` returns an error, map the error to a `DBError::InvalidUUID` error
-        // and early return from this function.
-        let uuid = todo!();
+        let uuid = sqlx::types::Uuid::try_parse(&answer_uuid)
+            .map_err(|_| DBError::InvalidUUID(format!("Invalid UUID: {}", answer_uuid)))?;
 
-        // TODO: Make a database query to delete an answer given the answer uuid.
-        // Here is the SQL query:
-        // ```
-        // DELETE FROM answers WHERE answer_uuid = $1
-        // ```
-        // If executing the query results in an error, map that error
-        // to a `DBError::Other` error and early return from this function.
+        sqlx::query!(
+            r#"
+                DELETE FROM answers WHERE answer_uuid = $1
+            "#,
+            uuid
+        ).execute(&self.db).await.map_err(|e| DBError::Other(Box::new(e)))?;
 
         Ok(())
     }
