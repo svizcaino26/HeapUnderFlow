285105c64a7beef60d44cfa46d1e6cb76cbd70e2
svizcaino26 <svizcaino26@gmail.com>
2026-06-23 19:08:15 -0500

    init stage 3 - step 2

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,77 +1,71 @@
-use crate::models::*;
-use axum::{response::IntoResponse, Json};
-use serde::Serialize;
+use axum::{extract::State, response::IntoResponse, Json};
+
+use crate::{models::*, AppState};
 
 mod handlers_inner;
 
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    Json(
-        QuestionDetail {
-            question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-            title: question.title,
-            description: question.description,
-            created_at: "2022-12-31 18:44:08.287442".to_string(),
-        }
-    )
+pub async fn create_question(
+    // Example of how to add state to a route. Note that we are using ".." to ignore the other fields in AppState.
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> impl IntoResponse {
+    Json(QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    Json(
-        vec![
-            QuestionDetail {
-                question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                title: "Newly Created Question".to_string(),
-                description: "Some description".to_string(),
-                created_at: "2022-12-31 18:44:08.287442".to_string(),
-            },
-            QuestionDetail {
-                question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                title: "Another Newly Created Question".to_string(),
-                description: "Some other  description".to_string(),
-                created_at: "2022-12-31 18:44:08.287442".to_string(),
-            },
-        ]
-    )
+pub async fn read_questions(// TODO: add questions_dao from app state as an argument
+) -> impl IntoResponse {
+    Json(vec![QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    ()
+pub async fn delete_question(
+    // TODO: add questions_dao from app state as an argument
+    Json(question_uuid): Json<QuestionId>,
+) {
+    // ...
 }
 
 // ---- CRUD for Answers ----
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    Json(
-        AnswerDetail {
-            answer_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-            question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-            content: "Some content".to_string(),
-            created_at: "2022-12-31 18:44:08.287442".to_string(),
-        }
-    )
+pub async fn create_answer(
+    // Example of how to add state to a route
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> impl IntoResponse {
+    Json(AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
 }
 
-pub async fn read_answers(Json(question_uuid): Json<QuestionId>) -> impl IntoResponse {
-    Json(
-        vec![
-            AnswerDetail {
-                answer_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                content: "Some content".to_string(),
-                created_at: "2022-12-31 18:44:08.287442".to_string(),
-            },
-            AnswerDetail {
-                answer_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_string(),
-                content: "Some content".to_string(),
-                created_at: "2022-12-31 18:44:08.287442".to_string(),
-            },
-        ]
-    )
+pub async fn read_answers(
+    // TODO: add answers_dao from app state as an argument
+    Json(question_uuid): Json<QuestionId>,
+) -> impl IntoResponse {
+    Json(vec![AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
 }
 
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) -> impl IntoResponse {
-    ()
+pub async fn delete_answer(
+    // TODO: add answers_dao from app state as an argument
+    Json(answer_uuid): Json<AnswerId>,
+) {
+    // ...
 }
